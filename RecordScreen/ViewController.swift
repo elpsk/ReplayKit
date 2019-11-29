c08dc52d4eca6cce8f63e12fc1fc49ae0041eead
Alberto Pasca <alberto.pasca@pirelli.com>
2019-11-29 17:35:17 +0100

    first commit

--- RecordScreen/ViewController.swift
+++ RecordScreen/ViewController.swift
@@ -7,14 +7,78 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+import AudioToolbox
+import ReplayKit
 
 
+class YourViewController: UIViewController {
+    let recorder = RPScreenRecorder.shared()
 }
 
+
+class ViewController: UIViewController {
+    
+    let recorder = RPScreenRecorder.shared()
+    
+    let colors: [UIColor] = [
+        UIColor.green,
+        UIColor.red,
+        UIColor.yellow,
+        UIColor.cyan,
+        UIColor.orange,
+        UIColor.blue
+    ]
+    
+    @IBOutlet weak var lblRandom: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { t in
+            self.lblRandom.text = "\(Int.random(in: 0 ..< 100))"
+            self.view.backgroundColor = self.colors[Int.random(in: 0 ..< self.colors.count)]
+        }
+        
+        setUpLongPressGestureRecognizer()
+    }
+    
+    func setUpLongPressGestureRecognizer() {
+        let longPressGesture = UILongPressGestureRecognizer.init(target: self, action: #selector(longPressAction(sender:)))
+        longPressGesture.minimumPressDuration = 0.5
+        longPressGesture.delaysTouchesBegan = true
+        
+        self.view.addGestureRecognizer(longPressGesture)
+    }
+    
+    @objc func longPressAction(sender: UILongPressGestureRecognizer) {
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        self.recording(state: sender.state == .began)
+    }
+    
+    func recording(state: Bool) {
+        if state {
+            guard recorder.isAvailable else { return }
+            recorder.startRecording{ (error) in
+                guard error == nil else { return }
+            }
+        } else {
+            recorder.stopRecording { (preview, error) in
+                guard let preview = preview else { return }
+
+                preview.modalPresentationStyle = .automatic
+                preview.previewControllerDelegate = self
+
+                self.present(preview, animated: true, completion: nil)
+            }
+        }
+    }
+    
+}
+
+extension ViewController : RPPreviewViewControllerDelegate {
+    
+    func previewControllerDidFinish(_ previewController: RPPreviewViewController) {
+        previewController.dismiss(animated: true, completion: nil)
+    }
+    
+}
